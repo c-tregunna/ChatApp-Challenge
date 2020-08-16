0c29b830d728ffb35adc92323d5de3dcc74757f9
Claire Tregunna <c.tregunna1980@gmail.com>
2020-08-16 21:02:01 +0100

    Added list of presidents and add message on click

--- js/app.js
+++ js/app.js
@@ -1,18 +1,64 @@
-const presidents = [
-    'JFK',
-    'Barack Obama',
-    'Twat Face',
-    'Richard Nixon'
-];
+/****************
+ * President names added to the aside section of site
+ */
 
-const presidentList = document.querySelector('.presidents');
+const presidentListOnline = document.querySelector('.p-online'); //ul with class presidents
+const presidentListIdle = document.querySelector('.p-idle');
+const presidentListAway= document.querySelector('.p-away');
 
-presidents.forEach(element => {
-    console.log(element);
-    let presidentName = document.createElement('LI');
-    presidentName.classList.add('president-list-items');
-    presidentName.textContent = element;
-    presidentList.appendChild(presidentName);
+presidents.forEach(president => {
+    let presidentName = document.createElement('LI'); //Create the li to add each president name to
+    presidentName.classList.add('president-list-items');// add the specific class to the li
+    presidentName.textContent = president.name; //add the individual name of each president to the li
+    if(president.isOnline) {
+        presidentListOnline.appendChild(presidentName); // append the li to the ul with class presidents
+    } else if (president.isIdle) {
+        presidentListIdle.appendChild(presidentName); // append the li to the ul with class presidents
+    } else {
+        presidentListAway.appendChild(presidentName); // append the li to the ul with class presidents
+    }
 
 });
+
+/********************
+ * Append a message to the main section
+ */
+
+let nameArray = presidents.map(a => a.name);
+console.log(nameArray);
+
+ function randomName (nameArray) {
+    return nameArray[Math.floor(Math.random()*nameArray.length)];
+
+ }
+
+ console.log(randomName(nameArray));
+
+const messageBlock = document.querySelector('.message-display');
+const messageInput = document.querySelector('input.addMessage');
+const submitButton = document.querySelector('button.submit');
+
+submitButton.addEventListener('click', () => {
+    let message = document.createElement("DIV");
+    message.classList.add('message');
+    let text = document.createElement('P');
+    let span = document.createElement('SPAN');
+    text.textContent = messageInput.value;
+    span.textContent = `By ${randomName(nameArray)}`;
+    text.appendChild(span);
+    message.appendChild(text);
+    messageInput.value = '';
+    messageBlock.appendChild(message);
+  });
+
+
+//This works if it is just a simple array of names
+// presidents.forEach(president => { // for each president do the below
+//     console.log(president.name); //logs each president to consol
+//     let presidentName = document.createElement('LI'); //Create the li to add each president name to
+//     presidentName.classList.add('president-list-items');// add the specific class to the li
+//     presidentName.textContent = president.name; //add the individual name of each president to the li
+//     presidentListOnline.appendChild(presidentName); // append the li to the ul with class presidents
+
+// });
 
---
+++ js/array.js
@@ -0,0 +1,111 @@
+const presidents = [
+    {
+        name: 'George W. Bush',
+        isOnline: false,
+        isAway: false,
+        isIdle: true
+      },
+      {
+        name: 'John F. Kennedy',
+        isOnline: true,
+        isAway: false,
+        isIdle: false
+      },
+      {
+        name: 'Donald J. Trump',
+        isOnline: true,
+        isAway: false,
+        isIdle: false
+      },
+      {
+        name: 'George Washington',
+        isOnline: true,
+        isAway: false,
+        isIdle: false
+      },
+      {
+        name: 'Thomas Jefferson',
+        isOnline: true,
+        isAway: false,
+        isIdle: false
+      },
+      {
+        name: 'Warren G. Harding',
+        isOnline: true,
+        isAway: false,
+        isIdle: false
+      },
+      {
+        name: 'Theodore Roosevelt',
+        isOnline: true,
+        isAway: false,
+        isIdle: false
+      },
+      {
+        name: 'Barack Obama',
+        isOnline: false,
+        isAway: false,
+        isIdle: true
+      },
+      {
+        name: 'Harry S. Truman',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'Abraham Lincoin',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'James Garfield',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'Chester A. Arthur',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'Abraham Lincoin',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'James Garfield',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'Chester A. Arthur',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'John Tyler',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'William McKinley',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      },
+      {
+        name: 'Lyndon B. Johnson',
+        isOnline: false,
+        isAway: true,
+        isIdle: false
+      }
+
+];
